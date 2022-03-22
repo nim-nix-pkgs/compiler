fe639a4ec78ae3ff9c9b5cd3733db39467f0f7f1
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:40 -0300

    chore: re-index ../nimpkgs/c/compiler/v1_4_0

--- v1_4_0/flake.nix
+++ v1_4_0/flake.nix
@@ -9,8 +9,8 @@
   
   inputs.src-compiler-v1_4_0.flake = false;
   inputs.src-compiler-v1_4_0.owner = "nim-lang";
-  inputs.src-compiler-v1_4_0.ref   = "refs/tags/v1.4.0";
-  inputs.src-compiler-v1_4_0.repo  = "Nim";
+  inputs.src-compiler-v1_4_0.ref   = "v1_4_0";
+  inputs.src-compiler-v1_4_0.repo  = "Nim.git";
   inputs.src-compiler-v1_4_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
